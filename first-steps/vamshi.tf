1618e537b7cf1f841920cff86295a95a787bbdd6
rajamodu <modurajasekhar@gmail.com>
2020-07-02 17:57:15 +0200

    raja commeted files in rajabranch

---
+++ first-steps/vamshi.tf
@@ -0,0 +1,3 @@
+first vamshi anae branch crate chesam next vamshi.tf file add chesi push chesam remote repo loki
+next git branch raja created from master account branch , later vamshi lo ki ochi git checkout raja ki veli
+akkada git pull origin vamshi ani chesthae file created in vamshi vi raja loki ochaie.
